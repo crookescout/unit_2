7c06edd127ebf39cee4abe20bf33dc7410fd9d59
scout <scout.crooke@ssfs.org>
2019-09-18 14:41:09 -0400

    worked on variable classwork exercises. Could not finish time one

--- sphere.py
+++ sphere.py
@@ -1,2 +1,23 @@
 # By Scout Crooke
 
+sphere_volume = 4 / 3 * 3.14 * (5**3)
+
+print("The volume of the sphere is", sphere_volume)
+
+num_books = 60
+book_price = 24.95 * .6 * num_books
+shipping = 3 + (.75 * (num_books - 1))
+total_cost = shipping + book_price
+
+print("The total wholesale cost for 60 copies is", total_cost)
+
+departure_in_minutes = (412/60)
+
+one_mile_in_minutes = 8.25
+three_miles_in_minutes = 7.2 * 3
+
+time_running = (2 * one_mile_in_minutes) + three_miles_in_minutes
+return_time = time_running + departure_in_minutes
+
+print(return_time)
+print("The he got home is", )
