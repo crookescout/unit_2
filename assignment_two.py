2a422d65a74f01afbe158d66d590d8382d9dbe6d
scout <scout.crooke@ssfs.org>
2019-09-24 11:06:23 -0400

    Finished unit 2 assignment option 1

--- assignment_two.py
+++ assignment_two.py
@@ -1,6 +1,5 @@
-# By Scout Crooke, 9/23/19, unit 2 assignment option 1
+# By Scout Crooke, 9/24/19, unit 2 assignment option 1
 
-# introductions
 print("Hi my name is BotBot!")
 name = input("What is your name?")
 print("Hi", name, "nice to meet you!")
@@ -8,21 +7,25 @@ print("Hi", name, "nice to meet you!")
 location = input("Where are you from?")
 print("Cool! I've always wanted to visit", location + ".", location, "sounds like a wonderful place to live!")
 
-number = int(input("What is your favorite number?"))
+number = float(input("What is your favorite number?"))
 my_number = number + 24
 print("Interesting! My favorite number is actually", my_number)
 
 car = input("What is your favorite car?")
 print("Woah! I've always wanted a", car)
 
-cost = int(input("How much does a car like that cost?"))
+cost = float(input("How much does a car like that cost?"))
 print("Wow!")
 
-interest = int(input("What is a reasonable yearly interest rate on a great car like that?"))
+interest = float(input("What is a reasonable yearly interest rate on a great car like that?"))
 print("Alright!")
+r = interest / 100 / 12  # converting to a decimal, then dividing by the number of month in a year
 
 years = int(input("And if you were to take out a loan to buy the car, how many years would you take the loan out for?"))
 n = 12 * years
-monthly_payment = (interest * cost) / (1 - ( 1 + interest) ** -n)
-total =
-print("If you bought a", car, "you would have a monthly payment of", "I hope that is reasonable with your budget. That would be a total of", total)
+monthly_payment = (r * cost) / (1 - (1 + r) ** -n)
+total_cost = years * 12 * monthly_payment
+print("Alright. So, if you bought a", car, "you would have a monthly payment of $" + str(monthly_payment))
+print("I hope that is reasonable with your budget. That would be a total of $" + str(total_cost))
+print("Well, I better get going now. It has been nice talking with you", name + "!")
+
